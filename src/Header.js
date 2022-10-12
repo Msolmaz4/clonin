24092efc7cb562294f438067123d08f58b3c06d9
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-10-12 22:24:39 +0200

    nav

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,14 @@
 import React from 'react'
 import ZoomOutIcon from '@mui/icons-material/ZoomOut';
+import './Header.css'
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
+
 
 const Header = () => {
   return (
@@ -15,7 +24,11 @@ const Header = () => {
         </div>
         
         <div className='header_right'>
-
+          <HeaderOption Icon= {HomeIcon} title='Home' />
+          <HeaderOption Icon= {SupervisorAccountIcon} title='My Network' />
+          <HeaderOption Icon= {BusinessCenterIcon} title='Jobs' />
+          <HeaderOption Icon= {ChatBubbleOutlineIcon} title='Messaging' />
+          <HeaderOption Icon= {NotificationsIcon} title='Notifications' />
         </div>
         
         
