c4f45486c630d950b0ffd756e4fca1b16acc24d6
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-10-13 23:59:09 +0200

    redux

--- src/features/counter/userSlice.js
+++ src/features/counter/userSlice.js
@@ -20,9 +20,11 @@ export const incrementAsync = createAsyncThunk(
   }
 );
 
-export const counterSlice = createSlice({
-  name: 'counter',
-  initialState,
+export const userSlice = createSlice({
+  name: 'user',
+  initialState:{
+    user:null,
+  },
   // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
     increment: (state) => {
@@ -54,7 +56,7 @@ export const counterSlice = createSlice({
   },
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { increment, decrement, incrementByAmount } = userSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
@@ -70,4 +72,4 @@ export const incrementIfOdd = (amount) => (dispatch, getState) => {
   }
 };
 
-export default counterSlice.reducer;
+export default userSlice.reducer;
